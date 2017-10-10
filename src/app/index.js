d5aa576fda771ec09b1fd54cba33791b78afb054
ReneeSlamka <renee.slamka@gmail.com>
2017-10-10 10:51:24 -0700

    Wokring basic angular app with directive (React js code commented out or neither work)

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Angular from 'angular';
 
-class Welcome extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
-}
-const element = <Welcome name="Sara" />;
-ReactDOM.render(
-  element,
-  document.getElementById('thingy-wrapper')
-);
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>Hello, {this.props.name}</h1>;
+//   }
+// }
+// const element = <Welcome name="Sara" />;
+// ReactDOM.render(
+//   element,
+//   document.getElementById('thingy-wrapper')
+// );
+
+angular.module('app',[])
+       .controller('TestCtrl', function($scope) {
+           $scope.titleStr = "This is a test";
+       })
+       .directive('test', function() {
+
+           return {
+               restrict: 'E',
+               scope: {
+                   phrase: '@'
+               },
+               template: '<p>{{phrase}}</p>'
+           };
+       });
+
+
+angular.element(function() {
+    angular.bootstrap(document, ['app']);
+});
